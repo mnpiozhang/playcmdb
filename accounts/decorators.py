b8340a370a0830694a8a3442678d52ca2609c791
cloudhzl <cloudhzl@163.com>
2017-04-18 11:48:32 +0800

    init

---
+++ accounts/decorators.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from functools import wraps
+from django.shortcuts import redirect,HttpResponse
+from .models import UserInfo
+# Create decorators
+#登录验证装饰器，失败则跳转至登录页面
+def is_login_auth(view_func):
+    @wraps(view_func)
+    def wrapper(request,*args, **kwargs):
+        if  request.session.get('login_auth',False):
+            return view_func(request,*args, **kwargs)
+        else:
+            return redirect('/accounts/login/')
+    return wrapper
+
+#webshell终端连接验证装饰器，非admin用户无法执行
+def is_admin_auth(view_func):
+    @wraps(view_func)
+    def wrapper(request,*args,**kwargs):
+        userauth = UserInfo.objects.get(username=request.session.get('username',None)).type.typename
+        #print userauth
+        #print type(userauth)
+        if userauth == "admin":
+            return view_func(request,*args, **kwargs)
+        else:
+            return HttpResponse("usertype is not admin , you have no permission to link remote host")
+    return wrapper
---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.views.decorators.csrf import csrf_exempt
+from .models import UserInfo
+from django.shortcuts import redirect,HttpResponse,render_to_response
+from .decorators import is_login_auth,is_admin_auth
+
+#登陆
+@csrf_exempt
+def login(request):
+# Create your views here.
+    ret = {'status':''}
+    if request.method == 'POST':
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        is_not_empty=all([username,password])
+        if is_not_empty:
+            count = UserInfo.objects.filter(username=username,password=password).count()
+            #判断输入用户名密码OK，则跳转到主页面
+            if count == 1:
+                request.session['username'] = username
+                request.session['login_auth'] = True
+                #log.info("user login : {}".format(username))
+                return redirect('/assets/index/')
+            else:
+                ret['status']='password error'
+        else:
+            ret['status']='can not empty'
+    return render_to_response('login.html',ret)
+
+#登出
+@is_login_auth
+def logout(request):
+    #log.info("user logout : {}".format(request.session['username']))
+    del request.session['login_auth']
+    del request.session['username']
+    return redirect("/web/login/")
