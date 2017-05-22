d1efe34498b07b84f5024ca11e7e05fae3f0be06
cloudhzl <cloudhzl@163.com>
2017-05-22 16:57:18 +0800

    更新

--- playcmdbLTE/wsgi.py
+++ playcmdbLTE/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for playcmdb project.
+WSGI config for playcmdbLTE project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playcmdb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playcmdbLTE.settings")
 
 application = get_wsgi_application()
