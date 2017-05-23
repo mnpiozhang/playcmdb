5084273130d7ba8ea8fac3ee75b7f390a34c8d25
cloudhzl <cloudhzl@163.com>
2017-05-23 14:17:12 +0800

    gx

--- playcmdb/urls.py
+++ playcmdb/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-"""playcmdbLTE URL Configuration
+"""playcmdb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
